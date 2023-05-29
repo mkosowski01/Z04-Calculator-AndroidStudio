cdc50f17f372dc47f9bb7233d29c0e0e172e6a03
Maciej Kosowski <maciej_kosowski@wp.pl>
2023-05-29 23:38:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package com.example.calculator;
+
+public class Calculator implements CalculatorInterface {
+    @Override
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    @Override
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    @Override
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    @Override
+    public double divide(double a, double b) {
+        return a / b;
+    }
+}
---
+++ app/src/main/java/com/example/calculator/CalculatorInterface.java
@@ -0,0 +1,8 @@
+package com.example.calculator;
+
+public interface CalculatorInterface {
+    double add(double a, double b);
+    double subtract(double a, double b);
+    double multiply(double a, double b);
+    double divide(double a, double b);
+}
